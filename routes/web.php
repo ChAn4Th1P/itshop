709112351de3fa8f26698c5a8e8e7ea1698cc55d
ChAn4Th1P <chanathip.s@kkumail.com>
2024-09-05 17:17:02 +0700

    ProductType

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use App\Http\Controllers\WebController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductTypeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/myshop', [WebController::class, 'myshop'])->name('web.myshop');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -18,6 +18,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
 });
+Route::get('/myshop', [WebController::class, 'myshop'])->name('web.myshop');
+Route::resource('product_types', ProductTypeController::class);
 
 require __DIR__.'/auth.php';
